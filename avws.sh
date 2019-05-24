fb221a9243b630c7e97b0f1af5eab687f90a2dfc
Kai <github@komadev.de>
2019-05-24 14:05:22 +0200

    Fix to run with Java >8u151 (tested on 8u211)

--- avws.sh
+++ avws.sh
@@ -6,6 +6,9 @@ if [ -z $4 ]; then
 fi
 
 TMPFILE=`mktemp`
+SECFILE=`mktemp`
+
+echo "Creating temporay jnlp and security addon files"
 
 LOGINID=`curl -s -k "https://$1/cgi-bin/kvm.cgi?&file=login" \
 -XPOST \
@@ -13,21 +16,25 @@ LOGINID=`curl -s -k "https://$1/cgi-bin/kvm.cgi?&file=login" \
 -H 'Content-Type: application/x-www-form-urlencoded' \
 -H 'Content-Length: 213' \
 -H 'Accept-Language: en-us' \
--H 'Host: 10.111.1.6' \
 -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.0.3 Safari/605.1.15' \
 -H 'Accept-Encoding: br, gzip, deflate' \
 -H 'Connection: keep-alive' \
 --data "action=SAVE&saveParms=login&filename=login&spcDevice=%28NULL%29&spcSocket=%28NULL%29&spcInlet=%28NULL%29&userindex=%28NULL%29&id=%28NULL%29&index=%28NULL%29&loginUsername=$2&loginPassword=$3&htmlLanguage=0"  | grep META | sed "s/.*userid=//" | sed "s/\".*//" `
 
-# -H 'Origin: https://10.111.1.6' \
-# -H 'Referer: https://$1/cgi-bin/kvm.cgi?&file=login' \
-
 echo Login ID for $2@https://$1 : $LOGINID
 
 curl -s -k https://$1/cgi-bin/kvm.cgi\?\&file\=jnlp\&userID\=$LOGINID\&index\=$4 >$TMPFILE
 
-javaws -J"-Djdk.tls.disabledAlgorithms=md2 -Djdk.certpath.disabledAlgorithms=md2" $TMPFILE
+cat >$SECFILE <<EOF
+jdk.certpath.disabledAlgorithms=MD2, RSA keySize < 1024
+jdk.tls.disabledAlgorithms=SSLv3, RC4
+EOF
 
-sleep 1
+echo Starting and waiting for JavaWS
+javaws -verbose -J"-Djava.security.properties=$SECFILE" $TMPFILE
+
+# Might be too little for slow systems
+sleep 8
 
 rm $TMPFILE
+rm $SECFILE
